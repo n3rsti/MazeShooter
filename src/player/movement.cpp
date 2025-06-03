6b983c35a51a56e3dd4f29196c3338e2f2500ba8
Krzysztof Witucki <n3rsti@protonmail.com>
2025-06-03 22:20:02 +0200

    fix corners

--- src/player/movement.cpp
+++ src/player/movement.cpp
@@ -1,40 +1,85 @@
 #include <cmath>
 
 #include "movement.h"
+#include "src/maze/maze.h"
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <cstdio>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+#include <ostream>
 #include <stdlib.h>
 #include <unordered_map>
+#include <vector>
 
-Movement::Movement(float speed) : speed(speed), x_move(0), z_move(0) {}
+Movement::Movement(float speed, Maze *maze)
+    : speed(speed), x_move(0), z_move(0), maze(maze) {}
 
 // Update movement based on key states
 void Movement::updateMovement(float x_rotation,
-                              std::unordered_map<int, bool> &keyStates) {
+                              std::unordered_map<int, bool> &keyStates,
+                              float xpos, float zpos) {
+
   float cos_camera_x = cos(x_rotation / 100);
   float sin_camera_x = sin(x_rotation / 100);
 
   x_move = 0;
   z_move = 0;
 
+  float tmp_x_move = 0;
+  float tmp_z_move = 0;
+
   if (keyStates[GLFW_KEY_W]) {
-    z_move += speed * cos_camera_x;
-    x_move -= speed * sin_camera_x;
+    tmp_z_move += speed * cos_camera_x;
+    tmp_x_move -= speed * sin_camera_x;
   }
   if (keyStates[GLFW_KEY_S]) {
-    z_move -= speed * cos_camera_x;
-    x_move += speed * sin_camera_x;
+    tmp_z_move -= speed * cos_camera_x;
+    tmp_x_move += speed * sin_camera_x;
   }
   if (keyStates[GLFW_KEY_A]) {
-    z_move += speed * sin_camera_x;
-    x_move += speed * cos_camera_x;
+    tmp_z_move += speed * sin_camera_x;
+    tmp_x_move += speed * cos_camera_x;
   }
   if (keyStates[GLFW_KEY_D]) {
-    z_move -= speed * sin_camera_x;
-    x_move -= speed * cos_camera_x;
+    tmp_z_move -= speed * sin_camera_x;
+    tmp_x_move -= speed * cos_camera_x;
   }
+  if (isValidMove(xpos + tmp_x_move * glfwGetTime(), zpos, tmp_x_move,
+                  tmp_z_move)) {
+    x_move = tmp_x_move;
+  }
+
+  if (isValidMove(xpos, zpos + tmp_z_move * glfwGetTime(), tmp_x_move,
+                  tmp_z_move)) {
+    z_move = tmp_z_move;
+  }
+}
+
+bool Movement::isValidMove(float x, float z, float x_direction,
+                           float z_direction) {
+
+  // printf("Angle X: %f, Angle Z: %f\n", x, z);
+  float blockSize = 2.0f;
+  // float offsetX = maze->width- 1;
+  //
+  // float offsetZ = maze->height - 1;
+  //
+  //
+  // float posX = x * blockSize - offsetX;
+  // float posZ = y * blockSize - offsetZ;
+  //
+  float radius = 0.1;
+  int my_x = (x + 1) / 2 + 35 + radius;
+  int my_z = (z + 1) / 2 + 35 + radius;
+
+  int my_x2 = (x + 1) / 2 + 35 - radius;
+  int my_z2 = (z + 1) / 2 + 35 - radius;
+  // print coordinates and maze value for this coordinate
+  // std::cout << "Cell: " << maze->get_cell(my_z, my_x);
+  return maze->get_cell(my_z, my_x) + maze->get_cell(my_z2, my_x2) +
+             maze->get_cell(my_z, my_x2) + maze->get_cell(my_z2, my_x) ==
+         0;
 }
--- src/player/movement.h
+++ src/player/movement.h
@@ -1,4 +1,5 @@
 
+#include "src/maze/maze.h"
 #include <cmath>
 
 #include <GL/glew.h>
@@ -8,16 +9,19 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <stdlib.h>
 #include <unordered_map>
+#include <vector>
 
 class Movement {
 public:
   float x_move;
   float z_move;
   float speed;
+  Maze *maze;
 
-  Movement(float speed);
+  Movement(float speed, Maze *maze);
 
   // Update movement based on key states
   void updateMovement(float x_rotation,
-                      std::unordered_map<int, bool> &keyStates);
+                      std::unordered_map<int, bool> &keyStates, float xpos, float zpos);
+    bool isValidMove(float x, float z, float x_direction, float z_direction);
 };
